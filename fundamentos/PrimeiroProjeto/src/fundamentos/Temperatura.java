62ae2c9890688b863f6cb7737e8cf6139e2d685c
djmjm <douglasjmjm@gmail.com>
2022-04-15 15:54:58 -0300

    Novo projeto, conversor temperatura (celsius para fahrenheit)

---
+++ fundamentos/PrimeiroProjeto/src/fundamentos/Temperatura.java
@@ -0,0 +1,9 @@
+package fundamentos;
+
+public class Temperatura {
+	public static void main(String[] args) {
+		double celsius = 12;
+		double fahrenheit = 1.8 * celsius + 32;
+		System.out.println(fahrenheit);
+	}
+}
