ec2574ef60936390ecdedac08e19b6bdc82c135b
djmjm <douglasjmjm@gmail.com>
2022-04-16 18:34:40 -0300

    Nova classe, testando tipos primitivos.

---
+++ fundamentos/PrimeiroProjeto/src/fundamentos/TiposPrimitivos.java
@@ -0,0 +1,26 @@
+package fundamentos;
+
+public class TiposPrimitivos {
+	public static void main(String[] args) {
+		
+		final byte NUMBER = 65;
+		
+		byte b = NUMBER;
+		short s = NUMBER;
+		int i = NUMBER;
+		long l = NUMBER;
+		char c = NUMBER;
+		boolean bo = true;
+		float f = NUMBER;
+		double d = NUMBER;
+		
+		System.out.println("Byte valor - " + b);
+		System.out.println("Short valor - " + s);
+		System.out.println("Int valor - " + i);
+		System.out.println("Long valor - " + l);
+		System.out.println("Char valor - " + c);
+		System.out.println("Boolean valor - " + bo);
+		System.out.println("Float valor - " + f);
+		System.out.println("Double valor - " + d);
+	}
+}
