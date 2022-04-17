7583c27f4110f3114481295801e9d00df011262f
djmjm <douglasjmjm@gmail.com>
2022-04-17 18:39:51 -0300

    Nova classe, exemplo acesso (notação ponto)

---
+++ fundamentos/PrimeiroProjeto/src/fundamentos/NotacaoPonto.java
@@ -0,0 +1,11 @@
+package fundamentos;
+
+public class NotacaoPonto {
+	public static void main(String[] args) {
+		System.out.println(
+				"Hello class ..."
+				.toUpperCase()
+				.concat("\ni am pleased to meet you!")
+				);
+	}
+}
