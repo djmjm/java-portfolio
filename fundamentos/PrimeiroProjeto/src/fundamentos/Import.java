5690deb066742ff15830c9d4a57324fe571faf9e
djmjm <douglasjmjm@gmail.com>
2022-04-17 18:51:58 -0300

    Nova classe, exemplo de import/include.

---
+++ fundamentos/PrimeiroProjeto/src/fundamentos/Import.java
@@ -0,0 +1,15 @@
+package fundamentos;
+
+import java.util.Date;
+
+public class Import {
+	public static void main(String[] args) {
+		java.lang.System.out.println(
+				new java.lang.String("Hello World ...")
+				+ "\n"
+				+ "Time:"
+				+ "\n"
+				+ new Date()
+				);
+	}
+}
