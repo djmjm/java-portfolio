c3cbf180ffc9368cfeaa7392d6c3bf2c0b45fff1
djmjm <douglasjmjm@gmail.com>
2022-04-18 18:22:15 -0300

    Nova classe, exemplo operadores atribuicao

---
+++ fundamentos/PrimeiroProjeto/src/fundamentos/OperadoresAtribuicao.java
@@ -0,0 +1,12 @@
+package fundamentos;
+
+public class OperadoresAtribuicao {
+	public static void main(String[] args) {
+		int numero = 1;
+		System.out.println(numero -= 1);
+		System.out.println(numero += 1);
+		System.out.println(numero *= 10);
+		System.out.println(numero /= 10);
+		System.out.println(numero %= 1);
+	}
+}
