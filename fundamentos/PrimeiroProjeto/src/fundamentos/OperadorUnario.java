0fd1a28a52cb56d0ef47e3bae185d11b842023ff
djmjm <douglasjmjm@gmail.com>
2022-04-18 18:34:06 -0300

    Nova classe, exemplo operadores unarios.

---
+++ fundamentos/PrimeiroProjeto/src/fundamentos/OperadorUnario.java
@@ -0,0 +1,13 @@
+package fundamentos;
+
+public class OperadorUnario {
+	public static void main(String[] args) {
+		int a = 1;
+		int b = 2;
+		
+		System.out.println(++a == b--);
+		System.out.println(a == b);
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
