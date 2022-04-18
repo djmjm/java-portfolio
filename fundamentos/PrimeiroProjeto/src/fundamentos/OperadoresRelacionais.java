4253c047f5a1f2f198922e6ea5bbab8e6d51e2b7
djmjm <douglasjmjm@gmail.com>
2022-04-18 18:06:41 -0300

    Nova classe, exemplo operadores relacionais.

---
+++ fundamentos/PrimeiroProjeto/src/fundamentos/OperadoresRelacionais.java
@@ -0,0 +1,19 @@
+package fundamentos;
+
+public class OperadoresRelacionais {
+	public static void main(String[] args) {
+		final int numero = 1;
+		boolean condicao_1 = (numero >= 1) ;
+		boolean condicao_2 = (numero <= 1) ;
+		boolean condicao_3 = (numero == 1) ;
+		boolean condicao_4 = (numero == numero / numero) ;
+		
+		boolean resultado = 
+				condicao_1 &&
+				condicao_2 &&
+				condicao_3 &&
+				condicao_4;
+		
+		System.out.println(resultado);
+	}
+}
