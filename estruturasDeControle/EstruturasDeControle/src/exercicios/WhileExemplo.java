7d3c029eaa2f8e97938ee79cbd4df90191e58e52
djmjm <douglasjmjm@gmail.com>
2022-04-24 11:40:34 -0300

    Nova classe, exemplo simples de loop (while)

---
+++ estruturasDeControle/EstruturasDeControle/src/exercicios/WhileExemplo.java
@@ -0,0 +1,13 @@
+package exercicios;
+
+public class WhileExemplo {
+	public static void main(String[] args) {
+		while(true) {
+			int contador = 0;
+			while(contador < 10) {
+				System.out.println(++contador);
+			}
+			return;
+		}
+	}
+}
