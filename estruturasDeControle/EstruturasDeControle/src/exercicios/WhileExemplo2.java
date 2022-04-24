99d0455061ed8d664d375f84f14fd01361808aa7
djmjm <douglasjmjm@gmail.com>
2022-04-24 12:08:55 -0300

    Nova classe, outro exemplo de loop (while)

---
+++ estruturasDeControle/EstruturasDeControle/src/exercicios/WhileExemplo2.java
@@ -0,0 +1,20 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class WhileExemplo2 {
+	public static void main(String[] args) {
+		Scanner leitor = new Scanner(System.in);
+		
+		String linha = leitor.nextLine();
+		while(!linha.equalsIgnoreCase("sair")) {
+			System.out.println(
+					"Usuário diz: "
+					+ linha
+					);
+			linha = leitor.nextLine();
+		}
+			
+		leitor.close();
+	}
+}
