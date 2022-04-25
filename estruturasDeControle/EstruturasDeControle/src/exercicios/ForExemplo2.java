7f295fc6b51086dadf4126fdf901a21f0c3a8568
djmjm <douglasjmjm@gmail.com>
2022-04-25 09:28:54 -0300

    Nova classe, exemplo loops aninhados (for)

--- estruturasDeControle/EstruturasDeControle/src/exercicios/ForExemplo2.java
+++ estruturasDeControle/EstruturasDeControle/src/exercicios/ForExemplo2.java
@@ -5,7 +5,7 @@ public class ForExemplo2 {
 		for(int i = 0; i < 10 ; i += 2) System.out.println(i);
 		for(int i = 0; !(i <= -10) ; i -= 2) System.out.println(i);
 		for(double i = 0.0; i < 0.10; i += 0.02) System.out.println(i);
-		for(boolean i = true, j = false	; !(i && j); j = i) {
+		for(boolean i = true, j = false; !(i && j); j = i) {
 			System.out.println(i);
 			System.out.println(j);
 		}
---
+++ estruturasDeControle/EstruturasDeControle/src/exercicios/ForExemplo3.java
@@ -0,0 +1,12 @@
+package exercicios;
+
+public class ForExemplo3 {
+	public static void main(String[] args) {
+		for(int i = 0; i < 10; i++) {
+			for(int j = 0; j < 10; j++) {
+				System.out.printf("[%d][%d] - ", i+1, j+1);
+			}
+			System.out.println("\n");
+		}
+	}
+}
