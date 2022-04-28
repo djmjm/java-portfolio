5d6c9d9f49a64a817696271f6a57c43c7a94597e
djmjm <douglasjmjm@gmail.com>
2022-04-28 14:37:50 -0300

    Refatorando classe Produto, implementação de novo método.

--- Exemplos/src/poo/Data.java
+++ Exemplos/src/poo/Data.java
@@ -5,22 +5,32 @@ public class Data {
 	private byte mes;
 	private short ano;
 	
+	private String texto;
+	
 	public Data() {
 		dia = 1;
 		mes = 6;
 		ano = 1995;
+		
+		formatarData();
 	}
 	
 	public Data(byte dia, byte mes, short ano) {
 		this.dia = dia;
 		this.mes = mes;
 		this.ano = ano;
+		
+		formatarData();
+	}
+	
+	private void formatarData() {
+		texto = dia + "/" + mes
+				+ "/" + ano;
 	}
 	
 	public void imprimirTela() {
 		System.out.println(
-				dia + "/" + mes
-				+ "/" + ano
+				texto
 				);
 	}
 }
