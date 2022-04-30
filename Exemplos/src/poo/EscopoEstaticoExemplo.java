3484484eab41ab9c8e8ec4b710263052c04a766c
djmjm <douglasjmjm@gmail.com>
2022-04-30 08:09:50 -0300

    Nova classe, exemplo sobre escopo de atributos (classe x objeto).

---
+++ Exemplos/src/poo/EscopoEstaticoExemplo.java
@@ -0,0 +1,19 @@
+package poo;
+
+public class EscopoEstaticoExemplo {
+	public int atributo_objeto;
+	public static int atributo_classe;
+	
+	public EscopoEstaticoExemplo() {
+		atributo_classe = 1;
+		atributo_objeto = 1;
+	}
+	
+	public void mudarAtributoClasse(int atributo_classe){
+		this.atributo_classe = atributo_classe;
+	}
+	
+	public void mudarAtributoObjeto(int atributo_objeto) {
+		this.atributo_objeto = atributo_objeto;
+	}
+}
---
+++ Exemplos/src/poo/EscopoEstaticoExemploTeste.java
@@ -0,0 +1,36 @@
+package poo;
+
+public class EscopoEstaticoExemploTeste {
+	public static void main(String[] args) {
+		EscopoEstaticoExemplo objeto_1 = new EscopoEstaticoExemplo();
+		EscopoEstaticoExemplo objeto_2 = new EscopoEstaticoExemplo();
+		
+		System.out.println(objeto_1.atributo_objeto + " - " +
+						   objeto_1.atributo_classe
+						);
+		
+		System.out.println(objeto_2.atributo_objeto + " - " +
+				   		   objeto_2.atributo_classe
+				);
+		
+		objeto_1.mudarAtributoClasse(2);
+		
+		System.out.println(objeto_1.atributo_objeto + " - " +
+				   		   objeto_1.atributo_classe
+				);
+
+		System.out.println(objeto_2.atributo_objeto + " - " +
+				   		   objeto_2.atributo_classe
+				);
+		
+		objeto_1.mudarAtributoObjeto(3);
+		
+		System.out.println(objeto_1.atributo_objeto + " - " +
+		   		   objeto_1.atributo_classe
+		);
+
+		System.out.println(objeto_2.atributo_objeto + " - " +
+				   		   objeto_2.atributo_classe
+				);
+	}
+}
