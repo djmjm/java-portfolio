e474a192e2076a9fa3c682ce3ae3975bb3fb251a
djmjm <douglasjmjm@gmail.com>
2022-04-30 09:59:00 -0300

    Nova classe, desafio escopo de variaveis.

---
+++ Exemplos/src/poo/EscopoDesafio.java
@@ -0,0 +1,12 @@
+package poo;
+
+public class EscopoDesafio {
+	
+	int a = 3;
+	static int b = 1983;
+	public static void main(String[] args) {
+		var runtime = new EscopoDesafio();
+		System.out.println(runtime.a);
+		System.out.println(b);
+	}
+}
