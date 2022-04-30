9466e653645ffd5c2d38c2438f18823a528b54c5
djmjm <douglasjmjm@gmail.com>
2022-04-30 10:38:07 -0300

    Nova classe, exemplo inicializador padrão (tipos primitivos).

---
+++ Exemplos/src/poo/InicializadorVariavelPadraoExemplo.java
@@ -0,0 +1,14 @@
+package poo;
+
+public class InicializadorVariavelPadraoExemplo {
+	int v_int;
+	double v_double;
+	char v_char;
+	float v_float;
+	boolean v_boolean;
+	String v_string;
+	
+	public InicializadorVariavelPadraoExemplo() {
+		;
+	}
+}
---
+++ Exemplos/src/poo/InicializadorVariavelPadraoExemploTeste.java
@@ -0,0 +1,15 @@
+package poo;
+
+public class InicializadorVariavelPadraoExemploTeste {
+	public static void main(String[] args) {
+		InicializadorVariavelPadraoExemplo runtime =
+				new InicializadorVariavelPadraoExemplo();
+		
+		System.out.println(runtime.v_int);
+		System.out.println(runtime.v_float);
+		System.out.println(runtime.v_double);
+		System.out.println(runtime.v_char);
+		System.out.println(runtime.v_boolean);
+		System.out.println(runtime.v_string);
+	}
+}
