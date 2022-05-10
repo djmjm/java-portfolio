2a3d3408d7b1b0433fbe84c9b97d4ecc4654e72f
djmjm <douglasjmjm@gmail.com>
2022-05-10 06:54:07 -0300

    Corrigindo erro de herança poopatterns.encapsulamento.grupo2.Pessoa1.

--- Exemplos/src/poopatterns/encapsulamento/grupo2/Pessoa1.java
+++ Exemplos/src/poopatterns/encapsulamento/grupo2/Pessoa1.java
@@ -1,6 +1,6 @@
 package poopatterns.encapsulamento.grupo2;
 
-public class Pessoa1 extends poopatterns.encapsulamento.grupo1.Pessoa1{
+public class Pessoa1{
 	private poopatterns.encapsulamento.grupo1.Pessoa1 
 		conhecido = new poopatterns.encapsulamento.grupo1.Pessoa1();
 	
