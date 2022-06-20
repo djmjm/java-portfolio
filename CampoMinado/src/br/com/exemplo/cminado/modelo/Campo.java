57959fa9505c7e797d03f266e1b03d6c2d67d5ba
Douglas Janovik Maliszewski <douglasjmjm@gmail.com>
2022-06-20 11:11:18 -0300

    Modificações classe Campo, novos testes JUnit.

--- CampoMinado/src/br/com/exemplo/cminado/modelo/Campo.java
+++ CampoMinado/src/br/com/exemplo/cminado/modelo/Campo.java
@@ -6,6 +6,8 @@ package br.com.exemplo.cminado.modelo;
 import java.util.ArrayList;
 import java.util.List;
 
+import br.com.exemplo.cminado.excecao.ExplosaoException;
+
 public class Campo {
 	private final int linha;
 	private final int coluna;
@@ -18,8 +20,8 @@ public class Campo {
 				new ArrayList<Campo>();
 	
 	public Campo(int linha, int coluna){
-		this.linha = linha;
-		this.coluna = coluna;
+		this.linha = Math.max(linha, 0);
+		this.coluna = Math.max(coluna, 0);
 	}
 	
 	public boolean adicionarVizinho(int x, int y) {
@@ -51,4 +53,54 @@ public class Campo {
 		      return false; 
 		}
 	}
+	
+	public void alterarMinado() {
+		if( !aberto ) {
+			minado = !minado;
+		}
+	}
+	
+	public void alterarMarcacao() {
+		if( !aberto ) {
+			marcado = !marcado;
+		}
+	}
+	
+	public boolean abrir() {
+		if(!aberto && 
+		   !marcado) {
+			
+			aberto = true;
+			
+			if(minado) {
+				throw new ExplosaoException();
+			}
+			
+			if( vizinhancaSegura() ) {
+				vizinhos.forEach( v -> v.abrir() );
+			}
+			
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public boolean vizinhancaSegura() {
+		return vizinhos.stream().noneMatch(v -> v.minado);
+	}
+	
+	public boolean isMarcado() {
+		return marcado;
+	}
+	
+	public boolean isAberto() {
+		return aberto;
+	}
+	
+	public boolean isMinado() {
+		return minado;
+	}
+	
+	
 }
