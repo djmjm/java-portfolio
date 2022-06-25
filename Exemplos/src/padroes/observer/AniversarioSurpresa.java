45ecea822721ef57b5a38048c1e808a1ee0f261d
Douglas Janovik Maliszewski <douglasjmjm@gmail.com>
2022-06-25 16:09:09 -0300

    Retirando código inútil

--- Exemplos/src/padroes/observer/AniversarioSurpresa.java
+++ Exemplos/src/padroes/observer/AniversarioSurpresa.java
@@ -6,10 +6,6 @@ public class AniversarioSurpresa {
 	public static void main(String[] args) {
 		Porteiro porteiro = new Porteiro();
 		Namorada namorada = new Namorada();
-		UnaryOperator<ObservadorChegadaAniversariante> mae = e -> {
-			System.out.println("Sua mãe também está aqui!");
-			return e;
-		};
 		
 		porteiro.registrarObservador(namorada);
 		
