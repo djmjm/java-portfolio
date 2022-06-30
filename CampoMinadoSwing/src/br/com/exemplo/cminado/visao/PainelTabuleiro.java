c795e4901a5d5199956c36ec2a7f555f96b79686
Douglas Janovik Maliszewski <douglasjmjm@gmail.com>
2022-06-30 10:01:31 -0300

    Implementação inicial da interface (JFrame e JPanel)

---
+++ CampoMinadoSwing/src/br/com/exemplo/cminado/visao/PainelTabuleiro.java
@@ -0,0 +1,11 @@
+package br.com.exemplo.cminado.visao;
+
+import javax.swing.JPanel;
+import br.com.exemplo.cminado.modelo.Tabuleiro;
+
+@SuppressWarnings("serial")
+public class PainelTabuleiro extends JPanel {
+	public PainelTabuleiro(Tabuleiro tabuleiro) {
+		//TODO impelmentar painel tabuleiro;;;
+	}
+}
