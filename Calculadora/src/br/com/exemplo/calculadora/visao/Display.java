45cf6498092dd6d5b96965524d4110cac5d0930b
Douglas Janovik Maliszewski <douglasjmjm@gmail.com>
2022-07-04 19:31:53 -0300

    Implementando classe display, ajustes interface.

--- Calculadora/src/br/com/exemplo/calculadora/visao/Display.java
+++ Calculadora/src/br/com/exemplo/calculadora/visao/Display.java
@@ -1,12 +1,28 @@
 package br.com.exemplo.calculadora.visao;
 
 import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class Display extends JPanel{
+	
+	private final JLabel label;
+	
 	public Display() {
-		setBackground(Color.BLUE);
+		setBackground( Color.WHITE );
+		label = new JLabel("1114,5");
+		label.setForeground(new Color(46, 49, 50) );
+		label.setFont(new Font("courier", 
+						  Font.PLAIN,
+						  30) 
+					);
+		
+		setLayout( new FlowLayout(FlowLayout.RIGHT) );
+		
+		add(label);
 	}
 }
