fd6102d192d879411c9610d914a062f3238b89f0
Douglas Janovik Maliszewski <douglasjmjm@gmail.com>
2022-07-04 18:50:11 -0300

    Nova classe display e teclado, configurando interface.

--- Calculadora/src/br/com/exemplo/calculadora/visao/Calculadora.java
+++ Calculadora/src/br/com/exemplo/calculadora/visao/Calculadora.java
@@ -1,10 +1,16 @@
 package br.com.exemplo.calculadora.visao;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
+import javax.swing.border.Border;
 
 @SuppressWarnings("serial")
 public class Calculadora extends JFrame{
 	public Calculadora() {
+		
+		organizarLayout();
 		
 		setSize(232, 322);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -13,6 +19,18 @@ public class Calculadora extends JFrame{
 		
 	}
 	
+	private void organizarLayout() {
+		setLayout(new BorderLayout());
+		
+		Display display = new Display();
+		Teclado teclado = new Teclado();
+		
+		display.setPreferredSize(new Dimension(233, 60));
+		add(display, BorderLayout.NORTH);
+		add(teclado, BorderLayout.CENTER);
+		
+	}
+
 	public static void main(String[] args) {
 		new Calculadora();
 	}
---
+++ Calculadora/src/br/com/exemplo/calculadora/visao/Teclado.java
@@ -0,0 +1,12 @@
+package br.com.exemplo.calculadora.visao;
+
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class Teclado extends JPanel{
+	public Teclado() {
+		setBackground(Color.RED);
+	}
+}
