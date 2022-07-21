0fb42c91ff6fe62daf79f1f62188971d97afe98f
Douglas Janovik Maliszewski <douglasjmjm@gmail.com>
2022-07-21 16:06:24 -0300

    Classe AlterarUsuario, modificando dados (merge).

---
+++ JPA/src/main/java/modelo/basico/AlterarUsuario.java
@@ -0,0 +1,32 @@
+package modelo.basico;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class AlterarUsuario {
+	public static void main(String[] args) {
+		EntityManagerFactory entityManagerFactory =
+				Persistence.createEntityManagerFactory(
+							"Exemplos-jpa"
+						)
+				;
+		EntityManager entityManager =
+				entityManagerFactory.
+				createEntityManager()
+				;
+		
+		entityManager.getTransaction().begin();
+		
+		Usuario usuario =
+				entityManager.find(Usuario.class, 1L)
+				;
+		usuario.setNome("Meu novo nome");
+		entityManager.merge(usuario);
+		
+		entityManager.getTransaction().commit();
+		
+		entityManager.close();
+		entityManagerFactory.close();
+	}
+}
