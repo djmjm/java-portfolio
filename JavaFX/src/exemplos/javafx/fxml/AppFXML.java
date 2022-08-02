112ead97665ce9b366cc459e51d7f4707b00ba4d
Douglas Janovik Maliszewski <douglasjmjm@gmail.com>
2022-08-02 18:40:13 -0300

    Tela de login com FXML.

--- JavaFX/src/exemplos/javafx/fxml/AppFXML.java
+++ JavaFX/src/exemplos/javafx/fxml/AppFXML.java
@@ -1,6 +1,11 @@
 package exemplos.javafx.fxml;
 
+import java.net.URL;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class AppFXML 
@@ -8,6 +13,15 @@ extends Application{
 	@Override
 	public void start(Stage primaryStage) 
 			throws Exception {
+		URL arquivoFXML = getClass().
+				getResource("Login.fxml");
+		GridPane raiz = FXMLLoader.load(arquivoFXML);
+		
+		Scene cena = new Scene(raiz, 350, 400);
+		
+		primaryStage.setResizable(false);
+		primaryStage.setTitle("Tela de Login");
+		primaryStage.setScene(cena);
 		primaryStage.show();
 	}
 	
