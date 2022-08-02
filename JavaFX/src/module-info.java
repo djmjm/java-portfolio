732c5f4151175b2f3392937a01366e11b3390e5a
Douglas Janovik Maliszewski <douglasjmjm@gmail.com>
2022-08-02 18:21:40 -0300

    Configurando arquivo fxml.

--- JavaFX/src/module-info.java
+++ JavaFX/src/module-info.java
@@ -2,7 +2,9 @@ module exemplos.javafx {
 	requires javafx.base;
 	requires javafx.controls;
 	requires javafx.graphics;
+	requires javafx.fxml;
 	
 	opens exemplos.javafx.basico;
 	opens exemplos.javafx.layout;
+	opens exemplos.javafx.fxml;
 }
