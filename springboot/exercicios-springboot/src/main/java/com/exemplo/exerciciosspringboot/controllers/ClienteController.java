8489f4a22183ce9635e5a1a97d8a9a010e014a38
Douglas Janovik Maliszewski <douglasjmjm@gmail.com>
2022-08-03 16:38:57 -0300

    WebService retornando objeto.

---
+++ springboot/exercicios-springboot/src/main/java/com/exemplo/exerciciosspringboot/controllers/ClienteController.java
@@ -0,0 +1,17 @@
+package com.exemplo.exerciciosspringboot.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.exemplo.exerciciosspringboot.models.Cliente;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+	
+	@GetMapping("/qualquer")
+	public Cliente obterCliente() {
+		return new Cliente(1, "Paulo", "125.125.125-45");
+	}
+}
