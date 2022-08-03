e76620aa2217832a3ca075646e90fdc7872b26b0
Douglas Janovik Maliszewski <douglasjmjm@gmail.com>
2022-08-03 15:48:45 -0300

    Criando projeto springboot, importando dependências com Maven.

---
+++ springboot/exercicios-springboot/src/main/java/com/exemplo/exerciciosspringboot/ExerciciosSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.exemplo.exerciciosspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciciosSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciciosSpringbootApplication.class, args);
+	}
+
+}
---
+++ springboot/exercicios-springboot/src/test/java/com/exemplo/exerciciosspringboot/ExerciciosSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exemplo.exerciciosspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciciosSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
