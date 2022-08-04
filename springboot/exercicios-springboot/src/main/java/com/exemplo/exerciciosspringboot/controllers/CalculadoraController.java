a7d81f5ecc71a022c12fa62c78164a897eec78a4
Douglas Janovik Maliszewski <douglasjmjm@gmail.com>
2022-08-04 20:46:38 -0300

    Desafio calculadora.

---
+++ springboot/exercicios-springboot/src/main/java/com/exemplo/exerciciosspringboot/controllers/CalculadoraController.java
@@ -0,0 +1,28 @@
+package com.exemplo.exerciciosspringboot.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@RequestMapping("/calculadora")
+public class CalculadoraController {
+	
+	@GetMapping("/somar/{a}/{b}")
+	public String somar(
+			@PathVariable Integer a,
+			@PathVariable Integer b) {
+		a += b; return a.toString();
+	}
+	
+	@GetMapping("/subtrair")
+	public String subtrair(
+			@RequestParam(name="a") Integer a,
+			@RequestParam(name="b") Integer b) {
+		a -= b; return a.toString();
+	}
+}
