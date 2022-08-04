a4320bddf78c4f33fdc85d9202dcb67f1385dc5d
Douglas Janovik Maliszewski <douglasjmjm@gmail.com>
2022-08-04 20:20:02 -0300

    Requisição GET padrão, parametros webservice.

--- springboot/exercicios-springboot/src/main/java/com/exemplo/exerciciosspringboot/controllers/ClienteController.java
+++ springboot/exercicios-springboot/src/main/java/com/exemplo/exerciciosspringboot/controllers/ClienteController.java
@@ -3,6 +3,7 @@ package com.exemplo.exerciciosspringboot.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.exemplo.exerciciosspringboot.models.Cliente;
@@ -19,6 +20,12 @@ public class ClienteController {
 	@GetMapping("/{id}")
 	public Cliente obterClientePorId( @PathVariable int id) {
 		return new Cliente(id, "Maria", "333.666.999-12");
+	}
+	
+	@GetMapping
+	public Cliente obterClientePorIdDefault(
+			@RequestParam (name="id") int id) {
+		return new Cliente(id, "Default", "111.111.111-11");
 	}
 
 }
