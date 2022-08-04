9dcff0040bda36c3bb2b350671afb873841be565
Douglas Janovik Maliszewski <douglasjmjm@gmail.com>
2022-08-04 19:42:09 -0300

    Adicionando paramêtros para serviço (método GET).

--- springboot/exercicios-springboot/src/main/java/com/exemplo/exerciciosspringboot/controllers/ClienteController.java
+++ springboot/exercicios-springboot/src/main/java/com/exemplo/exerciciosspringboot/controllers/ClienteController.java
@@ -1,6 +1,7 @@
 package com.exemplo.exerciciosspringboot.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,11 @@ public class ClienteController {
 	@GetMapping("/qualquer")
 	public Cliente obterCliente() {
 		return new Cliente(1, "Anônimo", "125.125.125-45");
+	}
+	
+	@GetMapping("/{id}")
+	public Cliente obterClientePorId( @PathVariable int id) {
+		return new Cliente(id, "Maria", "333.666.999-12");
 	}
 
 }
