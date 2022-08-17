adc07fcc38a44fdbed19e1b478d833b9a3de1701
Douglas Janovik Maliszewski <douglasjmjm@gmail.com>
2022-08-17 14:33:21 -0300

    Refatorando classe connection.

--- sqlite/src/database/Connection.java
+++ sqlite/src/database/Connection.java
@@ -3,17 +3,44 @@ package database;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class Connection {
+public class Connection{
+	private String databaseName;
+	private String databaseURL;
+	
+	private String connectionURL;
 	private java.sql.Connection 
 			connection;
+
+	public Connection() 
+	throws SQLException{
+		this("database.db", "data/myDB/");
+	}
 	
-	public boolean checkConnection(){
-		String url = "jdbc:sqlite:data/myDB/database.db";
+	public Connection
+	( String databaseName, 
+	  String databaseURL ) 
+	throws SQLException{
+		
+		this.databaseName = databaseName;
+		this.databaseURL = databaseURL;
+		connectionURL = "jdbc:sqlite:" 
+				 		+ databaseURL + databaseName;
+		
+		boolean isConnectionOK = 
+				checkConnection();
+		
+		if( isConnectionOK ) {
+			connection = 
+					DriverManager.
+					getConnection(connectionURL);
+		}
+	}
+
+	private boolean checkConnection(){
+		String url = connectionURL;
 		
 		try {
-			this.connection = DriverManager.
-								getConnection(url)
-								;
+			DriverManager.getConnection(url);
 		}
 		catch(SQLException e) {
 			System.out.println(
@@ -21,6 +48,16 @@ public class Connection {
 			); return false;
 		}
 		return true;
+	}
+
+	public java.sql.Connection getConnection() {
+		return connection;
+	}
+	
+	@Override
+	public String toString() {
+		Boolean answer = checkConnection();
+		return answer.toString();
 	}
 	
 }
--- sqlite/src/test/ConnectionTest.java
+++ sqlite/src/test/ConnectionTest.java
@@ -1,9 +1,15 @@
 package test;
 
+import java.sql.SQLException;
+
 import database.Connection;
 
 public class ConnectionTest {
 	public static void main(String[] args) {
-		System.out.println(new Connection().checkConnection());
+		try {
+			System.out.println(new Connection() );
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 }
