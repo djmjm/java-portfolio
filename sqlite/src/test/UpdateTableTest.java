bda123f967cb3b9e11da4f1cc72ceaa313c78cdb
Douglas Janovik Maliszewski <douglasjmjm@gmail.com>
2022-08-22 16:53:15 -0300

    Exemplo simples update.

---
+++ sqlite/src/test/UpdateTableTest.java
@@ -0,0 +1,36 @@
+package test;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import database.ConnectionFactory;
+
+public class UpdateTableTest {
+	public static void main(String[] args) {
+		try {
+			
+			String name = "joao";
+			
+			Connection connection = 
+					ConnectionFactory.getConnection().
+									  getConnection();
+			
+			String sql = "update test "
+					+ "set name='douglas' "
+					+ "where name=? "
+				;
+			
+			PreparedStatement stmt = connection.prepareStatement(sql);
+			stmt.setString(1, name);
+			stmt.execute();
+			
+			System.out.println("OK");
+			connection.close();
+			}
+			catch(SQLException | IOException e) {
+				e.printStackTrace();
+			}
+	}
+}
