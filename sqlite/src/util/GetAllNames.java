1d57f023e588257f402376e62d02a3d5d00d7d18
Douglas Janovik Maliszewski <douglasjmjm@gmail.com>
2022-08-24 15:21:41 -0300

    Novo pacote, outras modificações.

--- sqlite/src/util/GetAllNames.java
+++ sqlite/src/util/GetAllNames.java
@@ -1,23 +1,27 @@
-package test;
+package util;
 
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import database.ConnectionFactory;
 
-public class QueryTest2 {
-	public static void main(String[] args) {
+public abstract class GetAllNames {
+	public static List<String> 
+		ListNames(String nameTable) {
+			
 		try {
 			Connection connection =
 					ConnectionFactory.getConnection().
 									  getConnection()
 					;
 			String sql =
-					"select * from test "
-				  + "where name like 'd%'"
+					"select * from " +
+					nameTable
 					;
 			Statement stmt = 
 					connection.createStatement()
@@ -26,15 +30,19 @@ public class QueryTest2 {
 					stmt.executeQuery(sql)
 					;
 			
+			List<String> list  = new  ArrayList<String>();
 			while(answer.next()) {
-				System.out.println(answer.getString("name"));
+				list.add(
+					answer.getString("name")
+				)
+				;
 			}
-			
-			connection.close();
+			return list;
 			
 		}
 		catch(SQLException | IOException e) {
 			e.printStackTrace();
+			return new ArrayList<String>();
 		}
 	}
 }
---
+++ sqlite/src/util/NamesUpdateToUppercase.java
@@ -0,0 +1,49 @@
+package util;
+
+import java.sql.PreparedStatement;
+import java.util.List;
+import java.sql.Connection;
+
+import database.ConnectionFactory;
+
+public class NamesUpdateToUppercase {
+	public static void main(String[] args) {
+		List<String> listNames = 
+				GetAllNames.ListNames("test")
+				;
+		
+		try {
+			while( !listNames.isEmpty() ) {
+				
+				var nameOrigin = listNames.get(0);
+				var nameNew = nameOrigin.toUpperCase();
+				
+				Connection connection = 
+						ConnectionFactory.getConnection().
+										  getConnection();
+				
+				String sql = "update test "
+						+ "set name=? "
+						+ "where name=? "
+					;
+				
+				PreparedStatement stmt = connection.prepareStatement(sql);
+				stmt.setString(1, nameNew);
+				stmt.setString(2, nameOrigin);
+				stmt.execute();
+				
+				System.out.println(
+						nameOrigin +
+						" --->>> " +
+						nameNew
+						);
+				connection.close();
+				
+				listNames.remove(0);
+			}
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
